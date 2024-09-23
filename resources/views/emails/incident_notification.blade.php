e6b38121f52ba9920c75d07be1e89c0c2051240d
= <=>
2024-09-23 13:13:45 +0800

    new template

--- resources/views/emails/incident_notification.blade.php
+++ resources/views/emails/incident_notification.blade.php
@@ -4,12 +4,12 @@
     <title>Incident Notification</title>
 </head>
 <body>
-    <img src="{{ asset('images/3.png') }}" alt="Logo" width="auto" height="50">
+    <img src="{{ asset('images/3.png') }}" alt="Logo" width="auto" height="30">
     <h1>New Incident Created</h1>
     <p>Hello {{ $user->name }},</p>
     <p>An incident with the title <b>{{ $incident->title }}</b> has been created.</p>
     <p>We will keep you updated on the progress of this incident.</p>
-    <p>Incident ID: <a href="{{route('incidents.show', $incident->id)}}">{{ $incident->incident_number }}</a></b></p>
+    Incident ID: <a href="/incidents/{{$incident->id}}/show" target="_blank">{{ $incident->incident_number }}</a>
     <p>Please log in to view the details.</p>
 </body>
 </html>
