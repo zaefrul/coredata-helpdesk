8fdf979864c3f7be785bb02f9182afd02f0a0b92
= <=>
2024-10-02 01:54:03 +0800

    fix console

--- routes/console.php
+++ routes/console.php
@@ -9,15 +9,10 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('pm:pm-reminder', PMReminder::class)
-    ->purpose('Remind agents of PM tasks that are due in 30, 15, and 7 days')
-    ->daily()
-    ->appendOutputTo(storage_path('logs/pm-reminder.log'));
-
 Schedule::command('contracts:check-end-dates')
     ->daily()
     ->appendOutputTo(storage_path('logs/contracts.log'));
 
-// Schedule::command('pm:pm-reminder')
-//     ->daily()
-//     ->appendOutputTo(storage_path('logs/pm-reminder.log'));
+Schedule::command('pm:pm-reminder')
+    ->daily()
+    ->appendOutputTo(storage_path('logs/pm-reminder.log'));
