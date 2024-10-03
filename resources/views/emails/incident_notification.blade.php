9e6f43dcbca44094e533f915b26dbd13a59bfbfb
= <=>
2024-10-04 01:31:38 +0800

    fix notification email

--- resources/views/emails/incident_notification.blade.php
+++ resources/views/emails/incident_notification.blade.php
@@ -6,7 +6,7 @@
 <body>
     <img src="{{ asset('images/3.png') }}" alt="Logo" width="auto" height="30">
     <h1>New Incident Created</h1>
-    <p>Hello {{ $user->designation ? $user->designation . ' ', '' }}{{ $user->name }},</p>
+    <p>Hello {{ $user->designation ? $user->designation . ' ' : '' }}{{ $user->name }},</p>
     <p>An incident with the title <b>{{ $incident->title }}</b> has been created.</p>
     <p>We will keep you updated on the progress of this incident.</p>
     <p>Incident ID: <a href="{{route('incidents.show', $incident->id)}}">{{ $incident->incident_number }}</a></p>
